9b62bea7a047ffa3ba4880564bd317a0919022bd
Ваше Имя <.email@example.com>
2025-09-16 13:49:48 +0300

    first commit

--- PROJECT/script.js
+++ PROJECT/script.js
@@ -1,6 +1,47 @@
-console.log('Hello, World');
-console.log('Это сообщение для отладки');
-console.warn('Это предупреждение');
-console.error('Это сообщение об ошибке');
 
-alert("hello word")
+// let age = 21 // өзгеретын мән
+// let name = "Bakytzhan"
+// const country ="Kazakhstan" // өзгермейтын мән
+
+// console.log ("name:",name )
+// console.log ("age",age)
+// console.log ("country",country)
+
+// age = 22
+// console.log("age:",age)
+
+// let car1 = 22
+// let car2 = "blue"
+// let car3 = false
+
+// console.log (car1,typeof car1);
+// console.log (car2,typeof car2);
+// console.log (car3,typeof car3);
+
+
+// let car4 = null;
+// console.log(car4, typeof car4);
+
+// let car5;
+// console.log(car5, typeof car5);
+
+// let car6 = "blue" * 10;
+// console.log(car6, typeof car6);
+
+// console.log (car1,car2,car3,car4,car5,car6)
+
+
+let name =prompt("Bakytzhan")
+console.log("hello",name ,prompt)
+
+
+let age = 17;
+if (age <=18) {
+    console.log("сен камелетке толмагансын")
+}else{
+    console.log("камелет жаска толдын")
+}
+
+
+
+
